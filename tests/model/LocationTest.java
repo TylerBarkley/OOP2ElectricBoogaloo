1c64efdcaf722f84570b6d40ec3438d715677436
Joshua Novick <jnovick@ufl.edu>
2017-02-16 08:26:31 -0500

    Add tests for ability to get field of view

--- tests/model/LocationTest.java
+++ tests/model/LocationTest.java
@@ -2,6 +2,8 @@ package model;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -178,5 +180,137 @@ public class LocationTest
 		loc=new Location(5,-3);
 		loc = loc.wrapAround(6, 6);
 		assertEquals(new Location(5, 3), loc);
+	}
+	
+	@Test
+	public void getLocationsAtRadius_radius1_6correctLocations(){
+		ArrayList<Location> actual=loc.getLocationsAtRadius(1);
+		ArrayList<Location> expected=new ArrayList<Location>();
+		expected.add(new Location(5,4));
+		expected.add(new Location(6,4));
+		expected.add(new Location(6,5));
+		expected.add(new Location(5,6));
+		expected.add(new Location(4,6));
+		expected.add(new Location(4,5));
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void getLocationsAtRadius_radius2_12correctLocations(){
+		ArrayList<Location> actual=loc.getLocationsAtRadius(2);
+		ArrayList<Location> expected=new ArrayList<Location>();
+		expected.add(new Location(5,3));
+		expected.add(new Location(6,3));
+		expected.add(new Location(7,3));
+		expected.add(new Location(7,4));
+		expected.add(new Location(7,5));
+		expected.add(new Location(6,6));
+		expected.add(new Location(5,7));
+		expected.add(new Location(4,7));
+		expected.add(new Location(3,7));
+		expected.add(new Location(3,6));
+		expected.add(new Location(3,5));
+		expected.add(new Location(4,4));
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void getAllLocationsWithinRadius_radius2_18correctLocations(){
+		ArrayList<Location> actual=loc.getAllLocationsWithinRadius(2);
+		ArrayList<Location> expected=new ArrayList<Location>();
+		expected.add(new Location(5,3));
+		expected.add(new Location(6,3));
+		expected.add(new Location(7,3));
+		expected.add(new Location(7,4));
+		expected.add(new Location(7,5));
+		expected.add(new Location(6,6));
+		expected.add(new Location(5,7));
+		expected.add(new Location(4,7));
+		expected.add(new Location(3,7));
+		expected.add(new Location(3,6));
+		expected.add(new Location(3,5));
+		expected.add(new Location(4,4));
+		expected.add(new Location(5,3));
+		expected.add(new Location(6,3));
+		expected.add(new Location(7,3));
+		expected.add(new Location(7,4));
+		expected.add(new Location(7,5));
+		expected.add(new Location(6,6));
+		expected.add(new Location(5,7));
+		expected.add(new Location(4,7));
+		expected.add(new Location(3,7));
+		expected.add(new Location(3,6));
+		expected.add(new Location(3,5));
+		expected.add(new Location(4,4));
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void getLocationsAtRadius_radius1WithWrapAround_6correctLocations(){
+		ArrayList<Location> actual=loc.getLocationsAtRadius(1, 6, 7);
+		ArrayList<Location> expected=new ArrayList<Location>();
+		expected.add(new Location(5,4));
+		expected.add(new Location(0,4));
+		expected.add(new Location(0,5));
+		expected.add(new Location(5,6));
+		expected.add(new Location(4,6));
+		expected.add(new Location(4,5));
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void getLocationsAtRadius_radius2WithWrapAround_12correctLocations(){
+		ArrayList<Location> actual=loc.getLocationsAtRadius(2, 6, 7);
+		ArrayList<Location> expected=new ArrayList<Location>();
+		expected.add(new Location(5,3));
+		expected.add(new Location(0,3));
+		expected.add(new Location(1,3));
+		expected.add(new Location(1,4));
+		expected.add(new Location(1,5));
+		expected.add(new Location(0,6));
+		expected.add(new Location(5,0));
+		expected.add(new Location(4,0));
+		expected.add(new Location(3,0));
+		expected.add(new Location(3,6));
+		expected.add(new Location(3,5));
+		expected.add(new Location(4,4));
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void getAllLocationsWithinRadius_radius2WithWrapAround_18correctLocations(){
+		ArrayList<Location> actual=loc.getAllLocationsWithinRadius(2, 6, 7);
+		ArrayList<Location> expected=new ArrayList<Location>();
+		expected.add(new Location(5,3));
+		expected.add(new Location(0,3));
+		expected.add(new Location(1,3));
+		expected.add(new Location(1,4));
+		expected.add(new Location(1,5));
+		expected.add(new Location(0,6));
+		expected.add(new Location(5,0));
+		expected.add(new Location(4,0));
+		expected.add(new Location(3,0));
+		expected.add(new Location(3,6));
+		expected.add(new Location(3,5));
+		expected.add(new Location(4,4));
+		expected.add(new Location(5,3));
+		expected.add(new Location(0,3));
+		expected.add(new Location(1,3));
+		expected.add(new Location(1,4));
+		expected.add(new Location(1,5));
+		expected.add(new Location(0,6));
+		expected.add(new Location(5,0));
+		expected.add(new Location(4,0));
+		expected.add(new Location(3,0));
+		expected.add(new Location(3,6));
+		expected.add(new Location(3,5));
+		expected.add(new Location(4,4));
+		
+		assertEquals(expected, actual);
 	}
 }
