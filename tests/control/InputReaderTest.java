d88dd86ec87107b08351ccd016f783ad8b38cc7a
Joshua Novick <jnovick@ufl.edu>
2017-03-08 14:22:53 -0500

    Implement Customizable Controls

---
+++ tests/control/InputReaderTest.java
@@ -0,0 +1,15 @@
+package control;
+
+import javax.swing.JFrame;
+
+public class InputReaderTest {
+	
+	//NOT A JUnit TEST. MUST TEST MANUALLY
+    public static void main(String[] args){
+    	JFrame frame=new JFrame();
+    	frame.setSize(100, 100);
+    	frame.addKeyListener(new InputReader(new UserControls()));
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	frame.setVisible(true);
+    }
+}
