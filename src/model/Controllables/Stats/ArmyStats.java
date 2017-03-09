1913ebf16d15ada35af0dfe1e1899cf16d81aed1
TylerBarkley <tylerb306@ufl.edu>
2017-03-09 13:37:22 -0500

    Added attributes and getters and setters for ArmyStats

--- src/model/Controllables/Stats/ArmyStats.java
+++ src/model/Controllables/Stats/ArmyStats.java
@@ -4,4 +4,58 @@ package model.Controllables.Stats;
  * Created by Tyler Barkley on 3/9/2017.
  */
 public class ArmyStats {
+
+    private int offensiveDamage;
+    private int defensiveDamage;
+    private int upkeep;
+    private int movement;
+    private int armor;
+
+    public int getOffensiveDamage() {
+        return offensiveDamage;
+    }
+
+    public void setOffensiveDamage(int offensiveDamage) {
+        this.offensiveDamage = offensiveDamage;
+    }
+
+    public int getDefensiveDamage() {
+        return defensiveDamage;
+    }
+
+    public void setDefensiveDamage(int defensiveDamage) {
+        this.defensiveDamage = defensiveDamage;
+    }
+
+    public int getUpkeep() {
+        return upkeep;
+    }
+
+    public void setUpkeep(int upkeep) {
+        this.upkeep = upkeep;
+    }
+
+    public int getMovement() {
+        return movement;
+    }
+
+    public void setMovement(int movement) {
+        this.movement = movement;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public void addStats(UnitStats unitStats){
+
+    }
+
+    public void removeStats(UnitStats unitStats){
+        
+    }
 }
