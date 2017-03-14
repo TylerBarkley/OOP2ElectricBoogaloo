46c4fe7f315f2584e9227cde04cdf86e6d28f02c
zrolston <zrolston@ufl.edu>
2017-03-14 16:23:36 -0400

    Added rough Research Commands

---
+++ src/model/WorkerResearchCommand.java
@@ -0,0 +1,25 @@
+package model;
+
+/**
+ * Created by zrgam_000 on 3/14/2017.
+ */
+public class WorkerResearchCommand implements ResearchCommand {
+
+    Technology technology;
+    int stat;
+
+    public WorkerResearchCommand(Technology technology, int type, int stat){
+        this.technology = technology;
+        this.stat = stat;
+    }
+
+    @Override
+    public int getCost() {
+        return 10;
+    }
+
+    @Override
+    public void execute() {
+        technology.editWorkerStats(stat);
+    }
+}
